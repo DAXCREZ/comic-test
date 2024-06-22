0955ff9fbe3501377836e64a169e6f15dcf8bcaa
Daxcrez <lubafa2014@gmail.com>
2024-06-22 13:59:38 -0500

    Feat: Guardado en localStorage

--- src/context/ComicProvider.tsx
+++ src/context/ComicProvider.tsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from 'react'
+import { createContext, useEffect, useState } from 'react'
 
 export interface RatedComic {
   id: number
@@ -19,19 +19,28 @@ interface RatingProviderProps {
 function RatingProvider({ children }: RatingProviderProps) {
   const [ratings, setRatings] = useState<RatedComic[]>([])
 
+  useEffect(() => {
+    const storedRatings = localStorage.getItem('comicRatings')
+
+    if (storedRatings) {
+      setRatings(JSON.parse(storedRatings) as RatedComic[])
+    }
+  }, [])
+
   const rateComic = (id: number, rating: number) => {
     setRatings((prevRatings) => {
       const existingRatingIndex = prevRatings.findIndex((r) => r.id === id)
+      let updatedRatings
 
       if (existingRatingIndex !== -1) {
-        const updatedRatings = [...prevRatings]
-
+        updatedRatings = [...prevRatings]
         updatedRatings[existingRatingIndex] = { id, rating }
-
-        return updatedRatings
       } else {
-        return [...prevRatings, { id, rating }]
+        updatedRatings = [...prevRatings, { id, rating }]
       }
+      localStorage.setItem('comicRatings', JSON.stringify(updatedRatings))
+
+      return updatedRatings
     })
   }
 
